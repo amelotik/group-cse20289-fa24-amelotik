6ca79ea85bc32c53a2373981bf6a95e00f3874d5
Alicia Melotik <amelotik@student11.cse.nd.edu>
2024-09-30 15:55:38 -0400

    HW03 - wrote code to create PNG and doc

--- hw/hw03/createreport.py
+++ hw/hw03/createreport.py
@@ -1,4 +1,41 @@
 #Alicia Melotik
 #amelotik@nd.edu
 
+import argparse
+import json
+import docx
+
+def createWordDoc(textFile):
+    doc = docx.Document()
+    with open(textFile, 'r') as f:
+        lines = f.read()
+    doc.add_paragraph(lines)
+    return doc
+
+def insertTable(statData, doc):
+    table = doc.add_table(rows=0, cols=2)
+    table.style = 'Table Grid'
+    for key, value in statData.items():
+        row = table.add_row().cells
+        row[0].text = key
+        row[1].text = str(value)
+
+def combineDocParts(textString, statDict, graphPNG, outputFile):
+    doc = createWordDoc(textString)
+    insertTable(statDict, doc)
+    doc.add_picture(graphPNG)
+    doc.save(outputFile)
+
+if __name__ == "__main__":  
+    parser = argparse.ArgumentParser()
+    parser.add_argument("textFile", help="text file to be read in")
+    parser.add_argument("image", help="the image to be read in")
+    parser.add_argument("outputFile", help="the name of the doc file to output the plot to")
+    #use the parser to get the entered arguments
+    args = parser.parse_args()
+
+
+    statDict = {'period': 0, 'interface': 0, 'num points': 0, 'min': 0, 'max': 0, 
+                'mean': 0, 'median': 0, 'std dev': 0, '10th percentile': 0, '90th percentile': 0}
+    combineDocParts(args.textFile, statDict, args.image, args.outputFile)
 
--- hw/hw03/plotdata.py
+++ hw/hw03/plotdata.py
@@ -1,4 +1,50 @@
 #Alicia Melotik
 #amelotik@nd.edu
 
+import argparse
+import json
+import matplotlib.pyplot as plt
+import numpy as np
 
+def dailyAverage(data, numDays):
+    totalsDict = {day: {'totaltput': 0, 'countDays': 0} for day in range(1, numDays+1)}
+
+    for elem in data:
+        #split year-month-day from first part of timestamp, then narrow down to just day
+        date = elem['timestamp'].split('T')[0].split('-')[2]
+        if date.startswith('0'):
+            intDate = int(date[1])
+        else:
+            intDate = int(date)
+        totalsDict[intDate]['totaltput'] += elem['tput_mbps']
+        totalsDict[intDate]['countDays'] += 1
+
+    avgDict = {day: 0 for day in range(1, numDays+1)}
+    for day in avgDict:
+        avgDict[day] = totalsDict[day]['totaltput']/totalsDict[day]['countDays']
+    return avgDict
+
+def createPlot(dataDict, outputFile):
+    dates = list(dataDict.keys())
+    averages = list(dataDict.values())
+    plt.bar(dates, averages, color='blue', width = 0.4)
+
+    plt.xlabel("Day")
+    plt.ylabel("Average Throughput (Mb/s)")
+    plt.title("Average Daily Throughput")
+
+    plt.savefig(outputFile)
+     
+
+if __name__ == "__main__":  
+    parser = argparse.ArgumentParser()
+    parser.add_argument("JSON", help="JSON file to be read in")
+    parser.add_argument("numDays", type=int, help="the number of days in the month")
+    parser.add_argument("outputFile", help="the name of the file to output the plot to")
+    #use the parser to get the entered arguments
+    args = parser.parse_args()
+
+    dataset = json.loads(open(args.JSON).read())
+    avg = dailyAverage(dataset, args.numDays)
+    print(avg)
+    createPlot(avg, args.outputFile)
